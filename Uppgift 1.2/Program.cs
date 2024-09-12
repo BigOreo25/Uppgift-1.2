88014c7f172d7f281d33a3888f7b43caab5b4bc0
BigOreo25 <alts07001@utb.vaxjo.se>
2024-09-12 12:30:42 +0200

    Add project files.

---
+++ Uppgift 1.2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace kreb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dator kaboom om 3 sekunder");
+            Console.WriteLine("3...2...1...");
+        }
+    }
+}
